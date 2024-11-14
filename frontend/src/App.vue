e40bee754e1c3fff7afb023abf0c50eb7f37b492
이민선 <woooooooow22@gmail.com>
2024-11-14 21:22:14 +0900

    feat: 네비게이션 바 생성

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import './assets/index.css'
 import { profileImg } from './assets/index'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <span>SSAFIT</span>
+  <header class="border p-10 flex flex-row justify-between items-center">
+    <nav class="w-full flex flex-row justify-between items-center h-98 text-xs font-bold">
+      <RouterLink :to="{ name: 'home' }">SSAFIT</RouterLink>
+      <div class="flex flex-row gap-x-4">
         <RouterLink :to="{ name: 'videos' }">운동영상</RouterLink>
         <RouterLink :to="{ name: 'exercise' }">운동기록</RouterLink>
         <RouterLink :to="{ name: 'createVideo' }">영상등록</RouterLink>
+      </div>
+      <div class="flex flex-row gap-x-1">
         <img :src="profileImg" alt="auth icon" />
-        <span>Login</span>
+        <RouterLink :to="{ name: 'login' }">Login</RouterLink>
         /
-        <span>Register</span>
-      </nav>
-    </div>
+        <RouterLink :to="{ name: 'signup' }">Register</RouterLink>
+      </div>
+    </nav>
   </header>
   <RouterView />
 </template>
