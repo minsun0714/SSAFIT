b0f92c28187af77ccff457837844b579372468e1
이민선 <woooooooow22@gmail.com>
2024-11-14 21:26:00 +0900

    refactor: 컴포넌트 분리

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,9 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import './assets/index.css'
-import { profileImg } from './assets/index'
+import { RouterView } from 'vue-router'
+import NavigationBar from './components/common/NavigationBar.vue'
 </script>
 
 <template>
-  <header class="border p-10 flex flex-row justify-between items-center">
-    <nav class="w-full flex flex-row justify-between items-center h-98 text-xs font-bold">
-      <RouterLink :to="{ name: 'home' }">SSAFIT</RouterLink>
-      <div class="flex flex-row gap-x-4">
-        <RouterLink :to="{ name: 'videos' }">운동영상</RouterLink>
-        <RouterLink :to="{ name: 'exercise' }">운동기록</RouterLink>
-        <RouterLink :to="{ name: 'createVideo' }">영상등록</RouterLink>
-      </div>
-      <div class="flex flex-row gap-x-1">
-        <img :src="profileImg" alt="auth icon" />
-        <RouterLink :to="{ name: 'login' }">Login</RouterLink>
-        /
-        <RouterLink :to="{ name: 'signup' }">Register</RouterLink>
-      </div>
-    </nav>
-  </header>
+  <NavigationBar />
   <RouterView />
 </template>
