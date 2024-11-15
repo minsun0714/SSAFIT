0f81e6ff206e6c7eab27257fcb1490722af9e4b7
이민선 <woooooooow22@gmail.com>
2024-11-15 10:28:41 +0900

    feat: 일시정지 가능하도록 구현

--- frontend/src/components/Exercise/TimerModal.vue
+++ frontend/src/components/Exercise/TimerModal.vue
@@ -8,6 +8,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '../../components/ui/dialog'
+
 import { pad } from '../../utils/helperFunction'
 import BlueButton from '../common/BlueButton.vue'
 import { ref } from 'vue'
@@ -19,7 +20,7 @@ const isRunning = ref(false) // 타이머 상태 (실행 중인지 여부)
 // 재생/일시 정지 기능
 const toggleTimer = () => {
   if (isRunning.value) {
-    stopTimer()
+    pauseTimer() // 일시정지 함수 호출
   } else {
     startTimer()
   }
@@ -35,14 +36,19 @@ const startTimer = () => {
   }
 }
 
-// 타이머 정지 및 초기화
-const stopTimer = () => {
+// 타이머 일시정지 (시간 초기화 없이 멈춤)
+const pauseTimer = () => {
   if (timer) {
     clearInterval(timer)
     timer = null
     isRunning.value = false
-    time.value = 0
   }
+}
+
+// 타이머 정지 및 초기화
+const resetTimer = () => {
+  pauseTimer() // 타이머 멈춤
+  time.value = 0 // 시간 초기화
 }
 </script>
 
@@ -61,31 +67,30 @@ const stopTimer = () => {
           <div class="font-bold text-4xl">
             <span>{{ pad(Math.floor(time / 3600)) }}</span>
             :
-            <span>{{ pad(Math.floor(time / 60)) }}</span>
+            <span>{{ pad(Math.floor((time % 3600) / 60)) }}</span>
             :
-            <span> {{ pad(time % 60) }}</span>
+            <span>{{ pad(time % 60) }}</span>
           </div>
           <div class="w-full flex flex-row justify-evenly p-6">
+            <!-- 일시정지 / 재생 버튼 -->
             <div
-              v-if="isRunning"
-              class="bg-blue-400 flex justify-center items-center gap-1 border rounded-full w-16 h-16 cursor-pointer"
+              :class="[
+                'bg-blue-400 flex justify-center items-center gap-1 border rounded-full w-16 h-16 cursor-pointer',
+                isRunning ? 'pause-icon' : 'play-icon',
+              ]"
               @click="toggleTimer"
             >
-              <span class="bg-white h-1/3 text-transparent rounded-sm">_</span>
-              <span class="bg-white h-1/3 text-transparent rounded-sm">_</span>
-            </div>
-            <div
-              v-else
-              class="bg-blue-400 flex justify-center items-center gap-1 border rounded-full w-16 h-16 cursor-pointer"
-              @click="toggleTimer"
-            >
+              <span v-if="isRunning" class="bg-white h-1/3 text-transparent rounded-sm">_</span>
+              <span v-if="isRunning" class="bg-white h-1/3 text-transparent rounded-sm">_</span>
               <span
+                v-else
                 class="ml-1 w-0 h-0 border-t-[10px] border-t-transparent border-b-[10px] border-b-transparent border-l-[20px] border-l-white"
               ></span>
             </div>
+            <!-- 정지 버튼 -->
             <div
               class="bg-blue-400 flex justify-center items-center gap-1 border rounded-full w-16 h-16 cursor-pointer"
-              @click="stopTimer"
+              @click="resetTimer"
             >
               <span class="bg-white h-1/3 w-1/3 text-transparent rounded-sm"></span>
             </div>
