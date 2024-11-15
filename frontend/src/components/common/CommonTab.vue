b6a4c87dfb10295eba34a21013ca3bcae4ac4967
이민선 <woooooooow22@gmail.com>
2024-11-15 17:23:58 +0900

    feat: 상태 관리 방식으로 탭 구현 (라우팅 방식으로 변경 예정)

---
+++ frontend/src/components/common/CommonTab.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{
+  tabs: string[]
+}>()
+
+const active = ref(0)
+</script>
+<template>
+  <ul class="h-24 flex justify-center items-center gap-16 text-xs font-bold">
+    <li
+      v-for="(tab, index) in tabs"
+      :key="tab"
+      class="cursor-pointer"
+      :class="index === active && 'text-blue-400'"
+      @click="active = index"
+    >
+      {{ tab }}
+    </li>
+  </ul>
+</template>
--- frontend/src/views/MyPageView.vue
+++ frontend/src/views/MyPageView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import CommonTab from '@/components/common/CommonTab.vue'
 import MyProfile from '@/components/MyPage/MyProfile.vue'
 </script>
 
 <template>
   <main>
     <MyProfile />
+    <CommonTab :tabs="['좋아요한 영상', '남긴 후기', '정보관리']" />
   </main>
 </template>
