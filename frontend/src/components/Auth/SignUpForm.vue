471554f2b7c6fc62799ea2fb0c6070aa94990dea
이민선 <woooooooow22@gmail.com>
2024-11-17 22:13:49 +0900

    feat: 회원가입 기능 구현

--- frontend/src/components/Auth/SignUpForm.vue
+++ frontend/src/components/Auth/SignUpForm.vue
@@ -10,13 +10,15 @@ import { useRouter } from 'vue-router'
 
 import LottieAnimation from '@/components/Auth/LottieAnimation.vue'
 import { LOTTIE_ANIMATION_PATH } from '@/utils/enum'
+import MemberApiFacade from '@/api/apiFacade/MemberApiFacade'
 
 const router = useRouter()
 
 const formSchema = toTypedSchema(
   z.object({
     name: z.string().min(2).max(50),
-    userId: z.string().min(2).max(50),
+    memberId: z.string().min(2).max(50),
+    nickname: z.string().min(2).max(50),
     password: z.string().min(8).max(50),
     passwordConfirm: z.string().min(8).max(50),
   }),
@@ -26,13 +28,15 @@ const { handleSubmit } = useForm({
   validationSchema: formSchema,
 })
 
+const { mutate } = MemberApiFacade.useSignUp()
+
 const onSubmit = handleSubmit((value) => {
-  console.log(value)
+  mutate(value)
 })
 </script>
 
 <template>
-  <div class="flex flex-row justify-center h-[calc(100vh_-_98px)]">
+  <div class="flex flex-row justify-center h-screen">
     <div class="w-full">
       <LottieAnimation
         :animationPath="LOTTIE_ANIMATION_PATH.SIGN_UP"
@@ -60,9 +64,20 @@ const onSubmit = handleSubmit((value) => {
             </div>
           </FormItem>
         </FormField>
-        <FormField v-slot="{ componentField }" name="userId">
+        <FormField v-slot="{ componentField }" name="memberId">
           <FormItem>
             <FormLabel>아이디</FormLabel>
+            <FormControl>
+              <Input type="text" v-bind="componentField" />
+            </FormControl>
+            <div class="min-h-5">
+              <FormMessage />
+            </div>
+          </FormItem>
+        </FormField>
+        <FormField v-slot="{ componentField }" name="nickname">
+          <FormItem>
+            <FormLabel>닉네임</FormLabel>
             <FormControl>
               <Input type="text" v-bind="componentField" />
             </FormControl>
