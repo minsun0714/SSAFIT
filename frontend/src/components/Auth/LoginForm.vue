cab20cee834384d4f2728ccf0567ffd7e2b9b5f0
이민선 <woooooooow22@gmail.com>
2024-11-17 22:15:02 +0900

    feat: 로그인 기능 구현

--- frontend/src/components/Auth/LoginForm.vue
+++ frontend/src/components/Auth/LoginForm.vue
@@ -9,12 +9,13 @@ import BlueButton from '../common/BlueButton.vue'
 import { useRouter } from 'vue-router'
 import LottieAnimation from '@/components/Auth/LottieAnimation.vue'
 import { LOTTIE_ANIMATION_PATH } from '@/utils/enum'
+import AuthApiFacade from '@/api/apiFacade/AuthApiFacade'
 
 const router = useRouter()
 
 const formSchema = toTypedSchema(
   z.object({
-    userId: z.string().min(2).max(50),
+    memberId: z.string().min(2).max(50),
     password: z.string().min(8).max(50),
   }),
 )
@@ -23,28 +24,26 @@ const { handleSubmit } = useForm({
   validationSchema: formSchema,
 })
 
+const { mutate } = AuthApiFacade.useLogin()
+
 const onSubmit = handleSubmit((value) => {
-  console.log(value)
+  mutate(value)
 })
 </script>
 
 <template>
   <div class="flex flex-row justify-center h-[calc(100vh_-_98px)]">
     <div class="w-full">
-      <LottieAnimation
-        :animationPath="LOTTIE_ANIMATION_PATH.LOGIN"
-        :loop="true"
-        :autoplay="true"
-      />
+      <LottieAnimation :animationPath="LOTTIE_ANIMATION_PATH.LOGIN" :loop="true" :autoplay="true" />
     </div>
     <div class="w-full">
-      <div >
+      <div>
         <button v-text="'< Back'" class="p-8 text-gray-400" @click="router.back()"></button>
       </div>
       <form class="flex flex-col justify-center items-center space-y-6 p-10" @submit="onSubmit">
         <h1 class="font-bold text-xl w-[300px]">로그인</h1>
         <h2 class="text-gray-400 text-sm w-[300px]">로그인 후 다양한 운동 서비스를 즐겨보세요!</h2>
-        <FormField v-slot="{ componentField }" name="userId">
+        <FormField v-slot="{ componentField }" name="memberId">
           <FormItem>
             <FormLabel>아이디</FormLabel>
             <FormControl>
