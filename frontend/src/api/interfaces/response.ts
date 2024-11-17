683db89475188af57c259ae4fe8628d30c7345b0
이민선 <woooooooow22@gmail.com>
2024-11-17 21:59:36 +0900

    feat: 인증 구현에 필요한 인터페이스 선언

---
+++ frontend/src/api/interfaces/response.ts
@@ -0,0 +1,29 @@
+interface SignUpResponse extends Omit<Member, 'password'> {
+  profileImg: string
+  accessToken: string
+}
+
+interface LoginResponse {
+  grantType: 'Bearer'
+  accessToken: string
+}
+
+interface MemberInfoResponse extends Omit<Member, 'password'> {
+  profileImg: string
+  role: 'ROLE_ADMIN' | 'ROLE_USER'
+}
+
+interface ExerciseTimerResponse extends Exercise {
+  exerciseLogId: string
+  caloriesBurned: number
+  fatBurned: number
+}
+
+interface ExerciseCard {
+  title: string
+  cardIconUrl: string
+  cardType: ''
+  currentValue: number
+  lastValue: number
+  up: boolean
+}
