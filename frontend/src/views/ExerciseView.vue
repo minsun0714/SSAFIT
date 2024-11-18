fce71a761a5dc3861a5291b29792f4fbdb12bb96
이민선 <woooooooow22@gmail.com>
2024-11-18 22:38:57 +0900

    refactor: 불필요한 import 삭제

--- frontend/src/views/ExerciseView.vue
+++ frontend/src/views/ExerciseView.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import ExerciseCalendar from '@/components/Exercise/ExerciseCalendar.vue'
 import MonthlyInfoCardList from '../components/Exercise/MonthlyInfoCardList.vue'
-import TimerModal from '../components/Exercise/TimerModal.vue'
 </script>
 
 <template>
   <main>
     <MonthlyInfoCardList />
-
     <ExerciseCalendar />
   </main>
 </template>
