18199071ce22f5b4dfd192ce71f325a637b3b0bc
이민선 <woooooooow22@gmail.com>
2024-11-19 08:15:38 +0900

    feat: 테이블 및 페이지네이션 컴포넌트 적용

---
+++ frontend/src/components/Exercise/TimerModalExerciseTypeTable.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import {
+  Pagination,
+  PaginationEllipsis,
+  PaginationFirst,
+  PaginationLast,
+  PaginationList,
+  PaginationListItem,
+  PaginationNext,
+  PaginationPrev,
+} from '@/components/ui/pagination'
+import Button from '../ui/button/Button.vue'
+</script>
+
+<template>
+  <div>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>운동명</TableHead>
+          <TableHead>시간당 예상 칼로리 소모량</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell class="font-medium"> INV001 </TableCell>
+          <TableCell>Paid</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+    <Pagination v-slot="{ page }" :total="100" :sibling-count="1" show-edges :default-page="2">
+      <PaginationList v-slot="{ items }" class="flex items-center gap-1">
+        <PaginationFirst />
+        <PaginationPrev />
+
+        <template v-for="(item, index) in items">
+          <PaginationListItem v-if="item.type === 'page'" :key="index" :value="item.value" as-child>
+            <Button class="w-10 h-10 p-0" :variant="item.value === page ? 'default' : 'outline'">
+              {{ item.value }}
+            </Button>
+          </PaginationListItem>
+          <PaginationEllipsis v-else :key="item.type" :index="index" />
+        </template>
+
+        <PaginationNext />
+        <PaginationLast />
+      </PaginationList>
+    </Pagination>
+  </div>
+</template>
