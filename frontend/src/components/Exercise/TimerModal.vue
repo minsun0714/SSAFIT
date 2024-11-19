05ffeab7778db3d0cc1d16355ff5da355ecdc49a
이민선 <woooooooow22@gmail.com>
2024-11-19 19:33:18 +0900

    feat: 이전, 다음버튼 구현

--- frontend/src/components/Exercise/TimerModal.vue
+++ frontend/src/components/Exercise/TimerModal.vue
@@ -1,21 +1,31 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { Dialog, DialogContent, DialogTrigger } from '../../components/ui/dialog'
 
 import BlueButton from '../common/BlueButton.vue'
 import TimerModalExerciseTypeTable from './TimerModalExerciseTypeSearch.vue'
 import TimerModalTimer from './TimerModalTimer.vue'
 
+// 현재 표시할 컴포넌트를 상태로 관리
+const currentStep = ref<number>(0)
 </script>
 
 <template>
   <div>
     <Dialog>
-      <DialogTrigger class="border w-[300px] rounded-sm flex justify-center my-10"
-        ><BlueButton text="운동하기"
-      /></DialogTrigger>
+      <DialogTrigger class="border w-[300px] rounded-sm flex justify-center my-10">
+        <BlueButton text="운동하기" />
+      </DialogTrigger>
       <DialogContent class="flex flex-row">
-        <TimerModalExerciseTypeTable />
-        <TimerModalTimer />
+        <!-- currentStep에 따라 렌더링할 컴포넌트 전환 -->
+        <template v-if="currentStep === 0">
+          <TimerModalExerciseTypeTable />
+          <BlueButton text="다음" @click="currentStep += 1" />
+        </template>
+        <template v-else>
+          <TimerModalTimer />
+          <BlueButton text="이전" @click="currentStep -= 1" />
+        </template>
       </DialogContent>
     </Dialog>
   </div>
