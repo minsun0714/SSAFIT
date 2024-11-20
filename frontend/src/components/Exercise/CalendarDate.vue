d8faa7983369c570b5a44d80fdafdf56b14f41d2
이민선 <woooooooow22@gmail.com>
2024-11-20 23:47:33 +0900

    feat: 총계 데이터를 백엔드에서 전송하기로 결정 후 개별 데이터만 렌더링

--- frontend/src/components/Exercise/CalendarDate.vue
+++ frontend/src/components/Exercise/CalendarDate.vue
@@ -9,10 +9,10 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog'
+import { FaRegTrashAlt } from 'vue3-icons/fa'
 
 import { type DateValue } from '@internationalized/date'
 import { pad } from '@/utils/helperFunction'
-import { computed } from 'vue'
 
 const { weekDate, month } = defineProps<{
   weekDate: DateValue
@@ -24,40 +24,10 @@ const { data } = ExerciseLogApiFacade.useFetchExerciseLogsByDate(
 )
 
 const { mutate } = ExerciseLogApiFacade.useDeleteExerciseLog()
-
-const groupedData = computed(() => {
-  if (!data.value) return {}
-
-  return data.value.reduce(
-    (acc, log) => {
-      if (!acc[log.exerciseType]) {
-        acc[log.exerciseType] = {
-          totalExerciseTime: 0,
-          totalCaloriesBurned: 0,
-          logs: [],
-        }
-      }
-
-      acc[log.exerciseType].totalExerciseTime += log.exerciseTime
-      acc[log.exerciseType].totalCaloriesBurned += log.caloriesBurned
-      acc[log.exerciseType].logs.push(log)
-
-      return acc
-    },
-    {} as Record<string, { totalExerciseTime: number; totalCaloriesBurned: number; logs: any[] }>,
-  )
-})
-
-const totalExerciseTime = computed(() => {
-  return Object.values(groupedData.value || {}).reduce(
-    (total, group) => total + group.totalExerciseTime,
-    0,
-  )
-})
 </script>
 <template>
   <div class="w-full">
-    <Dialog>
+    <Dialog class="p-8">
       <DialogTrigger class="w-full text-end flex flex-col h-24">
         <CalendarCellTrigger :day="weekDate" :month="month" />
       </DialogTrigger>
@@ -66,42 +36,26 @@ const totalExerciseTime = computed(() => {
           <DialogTitle>
             <div>{{ weekDate.year }}년 {{ weekDate.month }}월 {{ weekDate.day }}일 운동</div>
           </DialogTitle>
-          <DialogDescription> 총 운동 시간 : {{ totalExerciseTime }}초 </DialogDescription>
+          <DialogDescription> 총 운동 시간 : {{ 0 }}초 </DialogDescription>
         </DialogHeader>
-        <!-- <div class="flex flex-row justify-evenly border w-full h-full">
-              <strong>{{ exerciseType }}</strong>
-              <p>{{ group.totalExerciseTime.toFixed(2) }}초</p>
-              <p>{{ group.totalCaloriesBurned.toFixed(2) }}kcal</p>
-            </div> -->
-        <ul v-if="Object.keys(groupedData).length">
+        <ul v-if="data?.length">
           <li
-            v-for="(group, exerciseType) in groupedData"
-            :key="exerciseType"
-            class="flex flex-col justify-between items-center"
+            v-for="log in data"
+            :key="log.exerciseLogId"
+            class="flex flex-row justify-between items-center w-full h-12 border-b"
           >
-            <div class="flex flex-row justify-evenly items-center border-b w-full h-12">
-              <strong>{{ exerciseType }}</strong>
-              <p>{{ group.totalExerciseTime.toFixed(2) }}초</p>
-              <p class="text-blue-400">{{ group.totalCaloriesBurned.toFixed(2) }}kcal</p>
+            <div class="flex flex-row gap-4">
+              <span>{{ log.exerciseType }}</span>
+              <span>{{ log.exerciseTime }}초</span>
+              <span class="text-blue-400">{{ log.caloriesBurned.toFixed(2) }}kcal</span>
             </div>
-            <!-- <ul>
-              <li
-                v-for="log in group.logs"
-                :key="log.exerciseLogId"
-                class="flex flex-row justify-evenly border w-[480px] h-full"
-              >
-                <p>{{ log.exerciseType }}</p>
-                <p>{{ log.exerciseTime.toFixed(2) }}초</p>
-                <p>{{ log.caloriesBurned.toFixed(2) }}kcal</p>
 
-                <button
-                  class="flex w-full justify-between"
-                  @click="() => mutate({ exerciseLogId: log.exerciseLogId as any })"
-                >
-                  삭제
-                </button>
-              </li>
-            </ul> -->
+            <button
+              class="flex justify-between"
+              @click="() => mutate({ exerciseLogId: log.exerciseLogId })"
+            >
+              <FaRegTrashAlt class="text-gray-400" />
+            </button>
           </li>
         </ul>
         <div v-else>해당 날짜의 운동 데이터가 없습니다.</div>
