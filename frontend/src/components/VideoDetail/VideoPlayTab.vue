c80d220ed7a06d80fc44cea1b8e3f32c09dda5fb
박범수 <pbs151241@gmail.com>
2024-11-20 15:37:31 +0900

    feat:Detail_picture

--- frontend/src/components/VideoDetail/VideoPlayTab.vue
+++ frontend/src/components/VideoDetail/VideoPlayTab.vue
@@ -3,7 +3,7 @@
       <div class="video-container">
         <iframe
         class="video-iframe"
-  src="https://www.youtube.com/embed/50WCSpZtdmA"
+  src="https://www.youtube.com/watch?v=4oGKJW-BdvY"
   frameborder="0"
   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
   allowfullscreen
