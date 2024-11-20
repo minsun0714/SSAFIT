80a015a173afdcb321b9b2f5148ff46c7e181b54
이민선 <woooooooow22@gmail.com>
2024-11-20 14:46:08 +0900

    style: 날짜별 운동기록 모달 스타일링

--- frontend/src/components/Exercise/CalendarDate.vue
+++ frontend/src/components/Exercise/CalendarDate.vue
@@ -25,13 +25,34 @@ const { data } = ExerciseLogApiFacade.useFetchExerciseLogsByDate(
 
 const { mutate } = ExerciseLogApiFacade.useDeleteExerciseLog()
 
-const totalExerciseTime = computed(() => {
-  const exerciseTimeData = data.value?.map((d) => d.exerciseTime) || []
-  return exerciseTimeData.reduce((a, b) => a + b, 0)
+const groupedData = computed(() => {
+  if (!data.value) return {}
+
+  return data.value.reduce(
+    (acc, log) => {
+      if (!acc[log.exerciseType]) {
+        acc[log.exerciseType] = {
+          totalExerciseTime: 0,
+          totalCaloriesBurned: 0,
+          logs: [],
+        }
+      }
+
+      acc[log.exerciseType].totalExerciseTime += log.exerciseTime
+      acc[log.exerciseType].totalCaloriesBurned += log.caloriesBurned
+      acc[log.exerciseType].logs.push(log)
+
+      return acc
+    },
+    {} as Record<string, { totalExerciseTime: number; totalCaloriesBurned: number; logs: any[] }>,
+  )
 })
-const totalKcalBurn = computed(() => {
-  const kcalBurnData = data.value?.map((d) => d.caloriesBurned) || []
-  return kcalBurnData.reduce((a, b) => a + b, 0)
+
+const totalExerciseTime = computed(() => {
+  return Object.values(groupedData.value || {}).reduce(
+    (total, group) => total + group.totalExerciseTime,
+    0,
+  )
 })
 </script>
 <template>
@@ -42,21 +63,45 @@ const totalKcalBurn = computed(() => {
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle
-            ><div>
-              {{ weekDate.year }}년 {{ weekDate.month }}월 {{ weekDate.day }}일 운동
-            </div></DialogTitle
-          >
-          <DialogDescription>
-            총 운동 시간 : {{ totalExerciseTime }} {{ totalKcalBurn }}kcal
-          </DialogDescription>
+          <DialogTitle>
+            <div>{{ weekDate.year }}년 {{ weekDate.month }}월 {{ weekDate.day }}일 운동</div>
+          </DialogTitle>
+          <DialogDescription> 총 운동 시간 : {{ totalExerciseTime }}초 </DialogDescription>
         </DialogHeader>
-        <ul v-if="data?.length">
-          <li v-for="d in data" :key="d.exerciseLogId">
-            {{ d.exerciseType }}
-            {{ d.exerciseTime }}초 {{ d.caloriesBurned }}kcal
-            {{ d.exerciseLogId }}
-            <button @click="() => mutate({ exerciseLogId: d.exerciseLogId as any })">삭제</button>
+        <!-- <div class="flex flex-row justify-evenly border w-full h-full">
+              <strong>{{ exerciseType }}</strong>
+              <p>{{ group.totalExerciseTime.toFixed(2) }}초</p>
+              <p>{{ group.totalCaloriesBurned.toFixed(2) }}kcal</p>
+            </div> -->
+        <ul v-if="Object.keys(groupedData).length">
+          <li
+            v-for="(group, exerciseType) in groupedData"
+            :key="exerciseType"
+            class="flex flex-col justify-between items-center"
+          >
+            <div class="flex flex-row justify-evenly items-center border-b w-full h-12">
+              <strong>{{ exerciseType }}</strong>
+              <p>{{ group.totalExerciseTime.toFixed(2) }}초</p>
+              <p class="text-blue-400">{{ group.totalCaloriesBurned.toFixed(2) }}kcal</p>
+            </div>
+            <!-- <ul>
+              <li
+                v-for="log in group.logs"
+                :key="log.exerciseLogId"
+                class="flex flex-row justify-evenly border w-[480px] h-full"
+              >
+                <p>{{ log.exerciseType }}</p>
+                <p>{{ log.exerciseTime.toFixed(2) }}초</p>
+                <p>{{ log.caloriesBurned.toFixed(2) }}kcal</p>
+
+                <button
+                  class="flex w-full justify-between"
+                  @click="() => mutate({ exerciseLogId: log.exerciseLogId as any })"
+                >
+                  삭제
+                </button>
+              </li>
+            </ul> -->
           </li>
         </ul>
         <div v-else>해당 날짜의 운동 데이터가 없습니다.</div>
