f333b65c814045b26dedfb7d885408c36d6f369b
이민선 <woooooooow22@gmail.com>
2024-11-24 08:37:59 +0900

    feat: dev tools 설치

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import NavigationBar from './components/common/NavigationBar.vue'
+import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
 </script>
 
 <template>
   <NavigationBar />
   <RouterView />
+  <VueQueryDevtools :initial-is-open="false" />
 </template>
