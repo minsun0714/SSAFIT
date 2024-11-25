2470e591d3a48fe396710321fcadb8904e567094
ParkBeomsu <pbs151241@gmail.com>
2024-11-25 09:07:41 +0900

    feat:member, review axios구현

---
+++ frontend/src/api/services/LikeVideosService.ts
@@ -0,0 +1,33 @@
+import api from '../apiClient'
+import type { LikeRequestDTO } from '../interfaces/request'
+import type { LikeResponseVO } from '../interfaces/response'
+import type { LikeVideos } from '../interfaces/common'
+
+class LikeVideosService {
+  private static path = '/api/likes'
+
+  // 좋아요 등록
+  static async addLike(requestDTO: LikeRequestDTO): Promise<LikeResponseVO> {
+    const response = await api.post(this.path, requestDTO)
+    return response.data
+  }
+
+  // 특정 멤버의 좋아요 조회
+  static async getLikesByMember(memberId: string): Promise<LikeVideos[]> {
+    const response = await api.get(`${this.path}/list/${memberId}`)
+    return response.data
+  }
+
+  // 특정 영상의 좋아요 조회
+  static async getLikesByVideo(videoId: string): Promise<LikeVideos[]> {
+    const response = await api.get(`${this.path}/${videoId}`)
+    return response.data
+  }
+
+  // 좋아요 삭제
+  static async removeLike(likeId: string): Promise<void> {
+    await api.delete(`${this.path}/${likeId}`)
+  }
+}
+
+export default LikeVideosService
