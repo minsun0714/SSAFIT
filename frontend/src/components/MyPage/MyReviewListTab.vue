ca6a875f24d95dbdd5621df50cdfcce4ca9f32b9
이민선 <woooooooow22@gmail.com>
2024-11-25 18:26:32 +0900

    feat: 마이페이지 댓글 목록 실제 데이터로 교체

--- frontend/src/components/MyPage/MyReviewListTab.vue
+++ frontend/src/components/MyPage/MyReviewListTab.vue
@@ -2,7 +2,8 @@
 import { FaStar, FaStarHalf } from 'vue3-icons/fa'
 import MyReviewContent from './MyReviewContent.vue'
 import { Ref, inject } from 'vue'
-const data = inject<Ref<ReviewResponse[] | undefined>>('reviewsInfo')
+import { ReviewResponseVO } from '@/api/interfaces/response'
+const data = inject<Ref<ReviewResponseVO[] | undefined>>('reviewsInfo')
 </script>
 
 <template>
@@ -10,32 +11,32 @@ const data = inject<Ref<ReviewResponse[] | undefined>>('reviewsInfo')
     <ul class="flex flex-col justify-center items-center w-full">
       <li
         v-for="review in data"
-        :key="review.id"
+        :key="review?.reviewId"
         class="w-2/3 h-full border-t border-b flex flex-col justify-center items-center text-sm text-gray-600"
       >
         <div class="flex flex-col items-center text-sm w-full pt-6 px-6">
           <div class="flex flex-row justify-between items-center w-full">
             <span class="flex flex-row items-center gap-2">
-              <img
+              <!-- <img
                 :src="review.user.profileImg"
                 alt="댓글 작성자의 프로필 사진"
                 class="border rounded-full h-8 w-8"
-              />
-              <h1>{{ review.user.username }}</h1>
+              /> -->
+              <h1>{{ review?.memberId }}</h1>
               ·
-              <p>{{ review.createdAt }}</p></span
+              <p>{{ review?.createdAt }}</p></span
             >
             <div class="grid grid-cols-5 gap-1">
               <span
                 class="flex flex-row"
-                v-for="(_, idx) in Math.floor(review.evaluation / 2)"
+                v-for="(_, idx) in Math.floor(review.rating / 2)"
                 :key="idx"
               >
                 <FaStar color="#ECBA0B" />
               </span>
               <span
                 class="flex flex-row"
-                v-for="(_, idx) in Math.floor(review.evaluation % 2)"
+                v-for="(_, idx) in Math.floor(review.rating % 2)"
                 :key="idx"
               >
                 <FaStarHalf color="#ECBA0B" />
